75285733f6b403cd53ee868ed67e8aca1d031600
solidliquidsmokeair <sjgogan@icloud.com>
2021-11-30 23:23:34 +1300

    build nav bar and title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <h1>hello</h1>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,21 @@ import './App.css';
 function Header() {
   return (
     <div className="navBar">
-      <h1>hello</h1>
+
+        <div className="title">
+
+        <h1 className="titleName">solid<br/>liquid<br/>smoke<br/>air</h1>
+
+        <div>
+            <ul className="menuItems">
+                <li className="menuItem">bio</li>
+                <li className="menuItem">contact</li>
+                <li className="menuItem">projects</li>
+                <li className="menuItem">aspirations</li>
+            </ul>
+        </div>
+
+      </div>
     </div>
   );
 }
